19b2ab6290357ddde97c056eb378fa60d4c3eb83
Tegveer Singh <tegveer2211@gmail.com>
2020-03-29 21:07:16 -0400

    added jokers to deck, updated to 108 total cards

--- Code/api/Deck.js
+++ Code/api/Deck.js
@@ -1,12 +1,22 @@
 //Initialize the deck of tiles
 class Deck {
     contructor() {
+        //Four player game works with 108
         const Deck=[];
         const color=["Red", "Blue", "Yellow", "Green"];
-        for (var i = 0; i <= color.length; i++) {
-            for (var j = 0; j < 13; j++) {
+        for (var i = 0; i <= color.length; i++) 
+        {
+            for (var j = 0; j < 26; j++) 
+            {
                 Deck.push(new Tile(color[i],j));
             }
+        }
+
+
+        //Add jokers
+        for (var i = 0; i < 4; i++) 
+        {
+        	Deck.push(new Tile("joker", i));
         }
     }
 }
